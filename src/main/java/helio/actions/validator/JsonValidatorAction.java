9e657ecf54329e2e714f77200d899faf8844d224
EmilioCrespoPeran <emiliocrespoperan@gmail.com>
2022-09-06 13:24:09 +0200

    Added JSON validator

---
+++ src/main/java/helio/actions/validator/JsonValidatorAction.java
@@ -0,0 +1,37 @@
+package helio.builder.siot.experimental.actions.module.validator;
+
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import helio.blueprints.Action;
+import helio.blueprints.exceptions.ActionException;
+
+/**
+ * Action class for Validator Module encharge of validating JSON schemas.
+ * Return a json as String value with the following structure:
+ * - "status": "ok" if it is a valid json or "error" instead.
+ * - "message" (Optional). A status description used for "error" status.
+ * 
+ * @author Emilio
+ * 
+ */
+public class JsonValidatorAction implements Action {
+
+	@Override
+	public void configure(JsonObject configuration) {}
+
+	@Override
+	public String run(String values) throws ActionException {
+		JsonObject response = new JsonObject();
+		try {
+			JsonParser.parseString(values).getAsJsonObject().toString();
+			response.addProperty("status", "ok");
+		}
+		catch (Exception e) {
+			response.addProperty("status", "error");
+			response.addProperty("message", "Invalid JSON format: " + e.getMessage());
+		}
+		return response.toString();
+	}
+
+}
